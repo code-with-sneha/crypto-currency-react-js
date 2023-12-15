2326fca79766d1f5bce9df01c6dd6336dae95ded
Sneha P <139157701+code-with-sneha@users.noreply.github.com>
2023-12-15 13:46:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,23 +18,5 @@ root.render(
   </React.StrictMode>
 );
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { BrowserRouter as Router } from 'react-router-dom';
 
-// import App from './App';
-// import store from './app/store';
-
-// import 'antd/dist/reset.css';
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Router>
-//       <Provider store={store}>
-//         <App />
-//       </Provider>
-//     </Router>
-//   </React.StrictMode>,
-//   document.getElementById('root'),
-// );
 
